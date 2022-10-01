319eca64d8a5c78172cbab4dd8ec026698291e59
garyho7043 <72981943+garyho7043@users.noreply.github.com>
2022-10-01 23:30:19 +0800

    Update DIPHW04.py

--- DIPHW04.py
+++ DIPHW04.py
@@ -9,7 +9,7 @@ def open_picture(name, img):
   cv2.destroyAllWindows()
   
 
-filename = "C:/Users/Max/Desktop/python/programming/DIP/hw3_coins2.jpg"
+filename = "./DIP/hw3_coins2.jpg"
 img = cv2.imread(filename)
 open_picture('origin', img)
 
@@ -21,28 +21,28 @@ gray = cv2.blur(gray, (3,3))
 
 ret, thresh = cv2.threshold(gray,0,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU) #OTSU+thresthod
 open_picture('thresh', thresh)
-cv2.imwrite('C:/Users/Max/Desktop/python/programming/DIP/thresh.jpg', thresh)
+cv2.imwrite('./DIP/thresh.jpg', thresh)
 
 # noise removal
 kernel = np.ones((3,3),np.uint8)
 opening = cv2.morphologyEx(thresh,cv2.MORPH_OPEN,kernel, iterations = 2) #more independent
 open_picture('opening', opening)
-cv2.imwrite('C:/Users/Max/Desktop/python/programming/DIP/opening.jpg', opening)
+cv2.imwrite('./DIP/opening.jpg', opening)
 opening = cv2.GaussianBlur(opening, (3, 3), 0)
 
 
 # sure background area
 sure_bg = cv2.dilate(opening,kernel,iterations=3)
 open_picture('sure_bg', sure_bg)
-cv2.imwrite('C:/Users/Max/Desktop/python/programming/DIP/sure_bg.jpg', sure_bg)
+cv2.imwrite('./DIP/sure_bg.jpg', sure_bg)
 
 # Finding sure foreground area
 dist_transform = cv2.distanceTransform(opening,cv2.DIST_L2,5) #make it different  judged on distance
 open_picture('dist_transform', dist_transform)
-cv2.imwrite('C:/Users/Max/Desktop/python/programming/DIP/dist_transform.jpg', dist_transform)
+cv2.imwrite('./DIP/dist_transform.jpg', dist_transform)
 ret, sure_fg = cv2.threshold(dist_transform,0.45*dist_transform.max(),255,0) 
 open_picture('sure_fg', sure_fg)
-cv2.imwrite('C:/Users/Max/Desktop/python/programming/DIP/sure_fg.jpg', sure_fg)
+cv2.imwrite('./DIP/sure_fg.jpg', sure_fg)
 
 # Finding unknown region
 sure_fg = np.uint8(sure_fg)
@@ -60,4 +60,4 @@ markers = cv2.watershed(img,markers)
 
 img[markers == -1] = [255,0,0]
 open_picture('output', img)
-cv2.imwrite('C:/Users/Max/Desktop/python/programming/DIP/output.jpg', img)
+cv2.imwrite('./DIP/output.jpg', img)
